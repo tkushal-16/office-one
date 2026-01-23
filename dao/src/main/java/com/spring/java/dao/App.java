e28778b54e35b095435e79b0d49ae1c314c9e247
tkushal-16 <kushaltkanaiaha7777@gmail.com>
2026-01-23 10:37:59 +0530

    Initial commit - Spring boot project setup

---
+++ dao/src/main/java/com/spring/java/dao/App.java
@@ -0,0 +1,13 @@
+package com.spring.java.dao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
