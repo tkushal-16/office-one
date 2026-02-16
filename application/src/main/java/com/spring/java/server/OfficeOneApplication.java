7b70194fb6e9e8bddf92d7eda8f2c1a4a8fd26b7
tkushal-16 <kushaltkanaiaha7777@gmail.com>
2026-02-16 23:03:22 +0530

    refactor project structure and file naming

--- application/src/main/java/com/spring/java/server/OfficeOneApplication.java
+++ application/src/main/java/com/spring/java/server/OfficeOneApplication.java
@@ -1,10 +1,13 @@
 package com.spring.java.server;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
  *
  */
-public class App 
+@SpringBootApplication
+public class OfficeOneApplication
 {
     public static void main( String[] args )
     {
