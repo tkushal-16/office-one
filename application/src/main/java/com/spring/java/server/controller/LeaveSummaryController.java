047e05cfcd27aa2ab7e5e616e681a371aa3ddb33
tkushal-16 <kushaltkanaiaha7777@gmail.com>
2026-03-14 13:57:45 +0530

    feat: implement leave controller

---
+++ application/src/main/java/com/spring/java/server/controller/LeaveSummaryController.java
@@ -0,0 +1,30 @@
+package com.spring.java.server.controller;
+
+
+
+import com.spring.java.common.user.User;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/leave/summary")
+@RequiredArgsConstructor
+public class LeaveSummaryController {
+
+    private final LeaveSummaryService leaveSummaryService;
+
+    @GetMapping("/monthly-summary")
+    public Object getOwnSummary(@AuthenticationPrincipal User user) {
+        return leaveSummaryService.getMonthlySummary(user, null);
+    }
+
+    @GetMapping("/monthly-summary/{userId}")
+    public Object getUserSummary(
+            @AuthenticationPrincipal User admin,
+            @PathVariable Long userId
+    ) {
+        return leaveSummaryService.getMonthlySummary(admin, userId);
+    }
+}
--- application/src/main/java/com/spring/java/server/controller/leaveController.java
+++
@@ -1,4 +0,0 @@
-package com.spring.java.server.controller;
-
-public class leaveController {
-}
